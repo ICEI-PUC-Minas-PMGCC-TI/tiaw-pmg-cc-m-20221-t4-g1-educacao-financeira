3a1abec11a86cee26b24d649b791f4f0cb4d0cc2
Mateus123finn <raphaelgomes671@gmail.com>
2022-06-12 14:21:31 -0300

    Ajustes finais Investimentos

--- codigo/scripts/edita_investimentos.js
+++ codigo/scripts/edita_investimentos.js
@@ -16,23 +16,23 @@ document.getElementById('inv').value = dados.id;
 document.getElementById('valor').value = dados.valor;
 
 function salvar(){
+    if(document.getElementById('inv').value && document.getElementById('valor').value){
+        for (let i = 0; i < dados_gerais.length; i++){
+            if(document.getElementById('inv').value == dados_gerais[i].id){
+                let aux = parseFloat(dados_gerais[i].valor) + parseFloat(document.getElementById('valor').value);
+                dados_gerais[i].valor = aux;
+                infos[logado].investimentos.splice(id,1);
+                localStorage.setItem('infos',JSON.stringify(infos));
+                window.location.href = "../pages/lista_investimentos.html";
+                return 0;
+            }
 
-    for (let i = 0; i < dados_gerais.length; i++){
+        }   
 
-        if(document.getElementById('inv').value == dados_gerais[i].id){
-            let aux = parseFloat(dados_gerais[i].valor) + parseFloat(document.getElementById('valor').value);
-            dados_gerais[i].valor = aux;
-            infos[logado].investimentos.splice(id,1);
-            localStorage.setItem('infos',JSON.stringify(infos));
-            window.location.href = "../pages/lista_investimentos.html";
-            return 0;
-        }
+        dados.id = document.getElementById('inv').value;
+        dados.valor = document.getElementById('valor').value;
 
+        localStorage.setItem('infos',JSON.stringify(infos));
+        window.location.href = "../pages/lista_investimentos.html";
     }
-
-    dados.id = document.getElementById('inv').value;
-    dados.valor = document.getElementById('valor').value;
-
-    localStorage.setItem('infos',JSON.stringify(infos));
-    window.location.href = "../pages/lista_investimentos.html";
 }
