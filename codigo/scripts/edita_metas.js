daa2252cd7e598589d8edeeeaf40813fe7431cc5
PedroHeinrichSP <103895487+PedroHeinrichSP@users.noreply.github.com>
2022-07-03 18:15:54 -0300

    TUDO

---
+++ codigo/scripts/edita_metas.js
@@ -0,0 +1,18 @@
+let id_raw = new URLSearchParams(window.location.search);
+let id = id_raw.get('item');
+let infos = JSON.parse(localStorage.getItem('infos'));
+let logado = sessionStorage.getItem('logado');
+let dados = infos[logado].metas[id];
+
+let user = JSON.parse(localStorage.getItem('banco'))
+
+document.getElementById('nome').value = dados.nome;
+document.getElementById('valor').value = dados.valor;
+
+function salvar(){
+    dados.nome = document.getElementById('nome').value;
+    dados.valor = document.getElementById('valor').value;
+
+    localStorage.setItem('infos',JSON.stringify(infos));
+    window.location.href = "../pages/lista_metas.html";
+}
