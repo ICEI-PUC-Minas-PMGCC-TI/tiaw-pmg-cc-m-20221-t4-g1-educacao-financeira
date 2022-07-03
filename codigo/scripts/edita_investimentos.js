6864cd1d942ef444cc4202353658be7752d5ca99
PedroHeinrichSP <103895487+PedroHeinrichSP@users.noreply.github.com>
2022-07-03 18:12:54 -0300

    a

---
+++ codigo/scripts/edita_investimentos.js
@@ -0,0 +1,33 @@
+let id_raw = new URLSearchParams(window.location.search);
+let id = id_raw.get('item');
+let infos = JSON.parse(localStorage.getItem('infos'));
+let logado = sessionStorage.getItem('logado');
+let dados = infos[logado].investimentos[id];
+let dados_gerais = infos[logado].investimentos
+
+let user = JSON.parse(localStorage.getItem('banco'))
+
+document.getElementById('inv').value = dados.id;
+document.getElementById('valor').value = dados.valor;
+
+function salvar(){
+    if(document.getElementById('inv').value && document.getElementById('valor').value){
+        for (let i = 0; i < dados_gerais.length; i++){
+            if(document.getElementById('inv').value == dados_gerais[i].id){
+                let aux = parseFloat(dados_gerais[i].valor) + parseFloat(document.getElementById('valor').value);
+                dados_gerais[i].valor = aux;
+                infos[logado].investimentos.splice(id,1);
+                localStorage.setItem('infos',JSON.stringify(infos));
+                window.location.href = "../pages/lista_investimentos.html";
+                return 0;
+            }
+
+        }   
+
+        dados.id = document.getElementById('inv').value;
+        dados.valor = document.getElementById('valor').value;
+
+        localStorage.setItem('infos',JSON.stringify(infos));
+        window.location.href = "../pages/lista_investimentos.html";
+    }
+}
---
+++ codigo/scripts/mostra_investimentos.js
@@ -0,0 +1,23 @@
+let id_raw = new URLSearchParams(window.location.search);
+let id = id_raw.get('item');
+let infos = JSON.parse(localStorage.getItem('infos'));
+let logado = sessionStorage.getItem('logado');
+let dados = infos[logado].investimentos[id];
+let desc = ["CDB","RDB","CRA","CRI","Debêntures","Debêntures incentivadas","LC","LCA","LCI","Letra Financeira","Letra de Câmbio","Títulos do Tesouro Direto",
+"Ações","Fundos imobiliários","Derivativos","Câmbio e moedas","ETFs","BDRs","Ouro"];
+let user = JSON.parse(localStorage.getItem('banco'));
+
+document.getElementById('nome').innerHTML = "<b>Tipo do Investimento:</b> "+desc[dados.id-1];
+document.getElementById('valor').innerHTML = "<b>Valor do Investimento:</b> R$ "+dados.valor;
+
+function deleta(){
+    if(window.confirm("Deseja apagar este Investimento ?")){
+        infos[logado].investimentos.splice(id,1);
+        localStorage.setItem('infos',JSON.stringify(infos));
+        window.location.href = '../pages/lista_investimentos.html';
+    }
+}
+
+function editar(){
+    window.location.href = "../pages/edita_investimentos.html?item="+id;
+}
